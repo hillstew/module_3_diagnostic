1e3cf9eea42ce13033b6a7a393f5ca2ddd1ba91b
Hillary Stewart <hillarystewart4427@gmail.com>
2019-12-09 11:10:26 -0700

    Add stub_request to index_spec and json

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Search Controller' do
   describe 'index' do
     it "a user should be able to search by house and see member in the OOTP" do
+      json_response = File.open('./app/fixtures/members_of_gryffindor.json')
+      stub_request(:get, "https://www.potterapi.com/v1/characters?key=#{ENV["POTTER_API_KEY"]}&orderOfThePhoenix=true&house=Gryffindor")
+        .to_return(status: 200, body: json_response)
+
       visit root_path
       
       select('Gryffindor', from: 'house')
